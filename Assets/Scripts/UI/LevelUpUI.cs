f4e1b2c5a1b6b095eda700122f262f86f74e27b1
Evanl202 <evanlin202@gmail.com>
2026-09-05 16:26:02 -0400

    Update LevelUpUI.cs
    Code for levelup

--- Assets/Scripts/UI/LevelUpUI.cs
+++ Assets/Scripts/UI/LevelUpUI.cs
@@ -1,3 +1,31 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+
+public class LevelUpUI : MonoBehaviour
+{
+    public GameObject levelUpPanel();
+
+    private void start()
+    {
+        levelUpPanel.SetActive(false);
+    }
+
+    public void ShowLevelUp()
+    {
+        levelUpPanel.SetActive(true);
+
+        Time.timeScale = 0f;
+
+        Debug.Log("Level up panel opened")
+    }
+
+    public void HideLevelUp()
+    {
+        levelUpPanel.SetActive(false)
+
+        Time.timeScale = 1f;
+
+        Debug.Log("Level up panel closed")
+    }
+}
