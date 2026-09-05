a2493dac534feb12c7319bfb3e4d40af07b1a5c4
Evanl202 <evanlin202@gmail.com>
2026-09-05 16:54:10 -0400

    Create UpgradeManager.cs
    Upgrades code

---
+++ Assets/Scripts/Core/UpgradeManager.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour
+{
+    public static UpgradeManager Instance;
+
+    [Header ("Player")]
+    public ShipMovement playerMovement;
+    public PlayerShip playerShip;
+    public Weapon playerWeapon;
+
+    [Header ("Upgrade Amount")]
+    public float speedIncrease = .10f;
+    public float reloadReduction = .10f;
+    public float healthIncrease = 20f;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+          Destroy(gameObject);  
+        }
+    }
+    
+    public void UpgradeSpeed()
+    {
+        if (playerMovement == null)
+        {
+            Debug.LogWarning("UpgradeManager: Player Movement not assigned");
+            return;
+        }
+        
+        playerMovement.maxSpeed *= 1f + speedIncrease;
+
+        Debug.Log("Speed Upgrade! New Max Speed: " + playerMovement.maxSpeed);
+    }
+
+    public void UpgradeReload()
+    {
+        if (playerWeapon == null)
+        {
+            Debug.LogWarning("UpgradeManager: Player Weapon not assigned");
+            return;
+        }
+        
+        playerWeapon.reloadTime *= 1f + reloadReduction;
+        
+        Debug.Log("Weapon Upgrade! New reload time: " + playerWeapon.reloadTime);
+        
+    }
+
+    public void UpgradeHealth()
+    {
+        if (playerShip == null)
+        {
+            Debug.LogWarning("UpgradeManager: Player Health not assigned");
+            return;
+        }
+        
+        playerShip.maxHealth += healthIncrease;
+        
+        Debug.Log("Health Upgrade! New Max Health: " + playerShip.maxHealth);
+        
+    }
+}
