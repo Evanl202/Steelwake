9ffd77aa743aacea8b34ae0e64782f6d8bdeab4f
Evanl202 <evanlin202@gmail.com>
2026-09-05 13:03:57 -0400

    Update ExperienceManager.cs
    Added the code

---
+++ Assets/Scripts/Core/ExperienceManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class ExperienceManager : MonoBehaviour
+{
+    public static ExperienceManager Instance;
+
+    [Header("Experience")]
+    public int currentLevel = 1;
+    public int currentXP = 0;
+    public int xpToNextLevel = 100;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+          Destroy(gameObject);  
+        }
+    }
+
+    public void AddXP()
+    {
+        currentXP += amount;
+
+        Debug.Log(
+            "Gained: " + amount +
+            "XP. Total XP: " + currentXP +
+            "/" + xpToNextLevel
+        )
+
+        CheckLevel()
+    }
+
+    public void CheckLevel()
+    {
+        if(currentXP >= xpToNextLevel)
+        {
+            LevelUp()
+
+        }
+    }
+
+    public void LevelUp()
+    {
+        currentXP -= xpToNextLevel;
+
+        currentXP++;
+
+        xpToNextLevel = Mathf.RoundToInt(
+            xpToNextLevel * 1.5f
+        );
+
+        Debug.Log(
+            "LEVEL UP! Current Level: " + currentLevel
+        );
+    }
+}
