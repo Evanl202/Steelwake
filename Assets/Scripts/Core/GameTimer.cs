2a7f4c69753156919f8a48d203da17b94e46331f
Evanl202 <evanlin202@gmail.com>
2026-09-07 13:20:59 -0400

    Create GameTimer.cs
    Code for ingame timer

---
+++ Assets/Scripts/Core/GameTimer.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using TMPro;
+
+public class GameTimer : MonoBehaviour
+{
+    [Header ("Timer")]
+    public float elaspedTime = 0f;
+
+    [Header ("UI")]
+    public TMP_Text timerText;
+
+    void Update()
+    {
+        if (GameManager.Instance != null && !GameManager.Instance.gameRunning)
+        {
+            return;
+        }
+        
+        elaspedTIme += Time.deltaTime;
+
+        UpdateTimerText();
+    }
+
+    private void UpdateTimerText()
+    {
+        int minutes = Mathf.FloorToInt(elapsedTime / 60f);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60f);
+
+        timerText.text = string.Format(
+            "{0:00}:{1:00}",
+            minutes, seconds
+        );
+    }
+}
