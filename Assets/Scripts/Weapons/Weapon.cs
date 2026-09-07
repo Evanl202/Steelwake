e701874998ccdece60652e57b3dfa84354f1862f
Evanl202 <evanlin202@gmail.com>
2026-09-07 12:54:25 -0400

    Update Weapon.cs
    Added gun rotation for aimming

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -6,6 +6,10 @@ public class Weapon : MonoBehaviour
     public GameObject shellPrefab;
     public Transform firingPoint;
 
+    [Header ("Gun Rotation")]
+    public Transform gunTransform;
+    public float reotationSpeed = 360f;
+
     [Header ("Damage")]
     public float damage = 25f;
 
@@ -16,6 +20,14 @@ public class Weapon : MonoBehaviour
 
     void Update()
     {
+
+        if (GameManager.Instance != null && !GameManager.Instance.gameRunning)
+        {
+            return;
+        }
+
+        HandleGunRotation();
+
         if (reloadTimer > 0f)
         {
             reloadTimer -= Time.deltaTime;
@@ -24,6 +36,48 @@ public class Weapon : MonoBehaviour
         if (Input.GetMouseButton(0) && reloadTimer <= 0f)
         {
             Fire();
+        }
+    }
+
+    private void HandleGunRotation()
+    {
+        if (gunTransform == null)
+            return;
+
+        Camera cam = Camera.main;
+
+        if (cam == null)
+            return;
+
+        //Horizontal plane at gun height
+        Plane oceanPlane = new Plane(
+            Vector3.up,
+            gunTransform.position
+        );
+
+        //Aimming
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition)
+
+        if (oceanPlane.Raycast(ray, out float distance))
+        {
+            Vector3 targetPoint = ray.GetPoint(distance);
+
+            Vector3 direction = targetPoint - gunTransform.position;
+
+            direction.y = 0f;
+
+            if (diection.sqrMagnitude < 0.001f)
+                return;
+
+            Quaternion targetRotation =
+                Quaternion.LookRotation(direction, Vector3.up);
+
+            //Rotate Turret
+            gunTransform.rotation = Quaternion.RotateTowards(
+                gunTransform.rotation,
+                targetRotation;
+                rotationSpeed * Time.deltaTime
+            );
         }
     }
 
