1b4155716a9682c2c3b148750f798f0e2840c799
Evanl202 <evanlin202@gmail.com>
2026-09-15 16:11:53 -0400

    Update CombatUI.cs
    Made it auto find the main gun

--- Assets/Scripts/UI/CombatUI.cs
+++ Assets/Scripts/UI/CombatUI.cs
@@ -4,17 +4,18 @@ using UnityEngine.UI;
 
 public class CombatUI : MonoBehaviour
 {
-    [Header ("Weapon")]
-    public Weapon weapon;
-
     [Header ("UI")]
     public TMP_Text weaponName;
     public TMP_Text ammoType;
     public TMP_Text reloadText;
     public Slider reloadBar;
 
+    private Weapon weapon;
+
     private void Update()
     {
+        FindActiveWeapon();
+
         if (weapon == null)
             return;
         
@@ -22,6 +23,28 @@ public class CombatUI : MonoBehaviour
         UpdateReload();
     }
 
+    private void FindActiveWeapon()
+    {
+        PlayerShip playerShip = FindAnyObjectByType<PlayerShip>();
+
+        if (playerShip == null)
+        {
+            weapon = null;
+            return;
+        }
+
+        Weapon playerWeapon = playerShip.GetComponentInChildren<Weapon>();
+
+        if (playerWeapon != null)
+        {
+            weapon = playerWeapon;
+        }
+        else
+        {
+            weapon = null;
+        }
+    }
+    
     private void UpdateWeaponInfo()
     {
         weaponName.text = "MAIN BATTERY";
