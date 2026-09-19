e57ecdbb27fd940018f1ee5459d5f2b0a14d43d7
Evanl202 <evanlin202@gmail.com>
2026-09-19 14:51:22 -0400

    Update CameraFollow.cs

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -6,7 +6,7 @@ public class CameraFollow : MonoBehaviour
     public Transform target;
 
     [Header ("Camera Position")]
-    public Vector3 offset = new Vector3(0f, 16f, -8f);
+    public Vector3 offset = new Vector3(0f, 24f, -24f);
 
     [Header ("Camera Movement")]
     public float followSpeed = 5f;
