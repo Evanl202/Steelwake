3af40f8df2595e8fffec83968237925b89c9586e
Evanl202 <evanlin202@gmail.com>
2026-09-23 16:01:13 -0400

    Update GameSettings.cs
    Added fps selected and persistence. Later will add audio and stuff

--- Assets/Scripts/Settings/GameSettings.cs
+++ Assets/Scripts/Settings/GameSettings.cs
@@ -1,12 +1,52 @@
 using UnityEngine;
+using TMPro;
 
 public class GameSettings : MonoBehaviour
 {
-    public int targetFPS = 60;
+    public TMP_Dropdown fpsDropdown;
+
+    private int[] fpsOptions = { 30, 60, 120, 144 };
 
     private void Awake()
     {
-        Application.targetFrameRate = targetFPS;
         DontDestroyOnLoad(gameObject);
+    }
+    
+    private void Start()
+    {
+        LoadFPS();
+    }
+
+    public void ApplyFPS()
+    {
+        int selectedFPS = fpsOptions[fpsDropdown.value];
+
+        Application.targetFrameRate = selectedFPS;
+
+        PlayerPrefs.SetInt("FPS", selectedFPS);
+        PlayerPrefs.Save();
+
+        Debug.Log("FPS set to " + selectedFPS);
+    }
+
+    private void LoadFPS()
+    {
+        int savedFPS = PlayerPrefs.GetInt("FPS", 60);
+
+        int index = 1;
+
+        for (int i = 0; i < fpsOptions.Length; i++)
+        {
+            if (fpsOptions[i] == savedFPS)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        fpsDropdown.value = index;
+        fpsDropdown.RefreshShownValue();
+
+        Application.targetFrameRate = savedFPS;
     }
 }
