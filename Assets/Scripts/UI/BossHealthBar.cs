3b897f6256d3d4ae2f605d5f08755f28b260d301
Evanl202 <evanlin202@gmail.com>
2026-09-23 14:06:23 -0400

    Create BossHealthBar.cs
    Added bossbar ui

---
+++ Assets/Scripts/UI/BossHealthBar.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BossHealthBar : MonoBehaviour
+{
+    public Slider healthSlider;
+    public TMP_Text healthText;
+
+    public EnemyShip boss;
+
+    private void Update()
+    {
+        if (boss == null)
+            return;
+
+        if (healthSlider == null || healthText == null)
+            return;
+
+        UpdateHealthBar();
+    }
+
+    public void SetBoss(EnemyShip newBoss)
+    {
+        boss = newBoss;
+    }
+
+    private void UpdateHealthBar()
+    {
+        float currentHealth = boss.CurrentHealth;
+        float maxHealth = boss.maxHealth;
+
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = currentHealth;
+
+        healthText.text =
+            Mathf.CeilToInt(currentHealth) +
+            " / " +
+            Mathf.CeilToInt(maxHealth);
+    }
+}
